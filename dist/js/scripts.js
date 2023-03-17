2c5c2bfcb0e10d07a10283d649b4ca0975356552
Victor Soto <alexandervictor550@gmail.com>
2023-03-16 20:43:26 -0600

    Setting up gh-pages package

--- dist/js/scripts.js
+++ dist/js/scripts.js
@@ -1,5 +1,5 @@
 /*!
-* Start Bootstrap - Resume v7.0.5 (https://startbootstrap.com/theme/resume)
+* Start Bootstrap - Resume v7.0.5 (https://victoralexander132.github.io/bootstrap-resume/)
 * Copyright 2013-2023 Start Bootstrap
 * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-resume/blob/master/LICENSE)
 */
