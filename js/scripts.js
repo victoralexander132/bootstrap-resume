63b7fc32c7078c9ebd3cbd1f05102bb791cefb0a
Victor Soto <vicsotoa@amazon.com>
2026-07-20 02:06:34 +0000

    Updates

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,6 @@
 /*!
-* Start Bootstrap - Resume v7.0.5 (https://victoralexander132.github.io/bootstrap-resume/)
-* Copyright 2013-2023 Start Bootstrap
+* Start Bootstrap - Victor Soto Alexander — Resume v7.0.5 (https://victoralexander132.github.io/bootstrap-resume/)
+* Copyright 2013-2026 Start Bootstrap
 * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-resume/blob/master/LICENSE)
 */
 //
